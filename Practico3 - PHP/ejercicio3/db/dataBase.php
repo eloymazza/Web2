11ae1cf584d2a60841232acb7df3f3ec004eb544
Eloy Mazza <eloymazza.ra@gmail.com>
2018-01-21 19:46:17 -0300

    Agregue auto create database

---
+++ Practico3 - PHP/ejercicio3/db/dataBase.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+class DataBase{
+
+    protected $db;
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;dbname=asistencia;charset=utf8','root', '');
+    }
+}
+
+class DBPreloader extends DataBase{
+
+
+    function __construct($paths, $sentences){
+        
+        parent::__construct();
+        for ($i=0; $i < count($paths); $i++) { 
+            $this->precargar($paths[$i], $sentences[$i]);
+        }
+        fopen('dbStart/preloaded.txt', 'w+');
+    }      
+
+    function precargar($path, $sentence){
+        $file = fopen($path, 'r');   
+        $log = array();
+        
+        while (!feof($file)){
+            $log = explode(';',fgets($file));
+            $this->insert($log,$sentence);
+        }    
+        fclose($file);
+    }
+    
+    function insert($log,$sentenceSQL){
+        $sentence = $this->db->prepare($sentenceSQL);
+        $sentence->execute($log);
+    }
+    
+}
+
+class DBInteractions extends DataBase{
+    
+
+
+
+}
+
+if(!file_exists('dbStart/preloaded.txt')){
+    $paths = array(
+        'dbStart/inscriptos.txt',
+        'dbStart/estudiantes.txt',
+        'dbStart/materias.txt'
+    );
+    $sentences = array(
+        0 =>'INSERT INTO inscriptos(dni,matematicas,web1,programacion1,ingles) VALUES (?,?,?,?,?)',
+        1 =>'INSERT INTO alumno(dni,nombre,apellido) VALUES (?,?,?)',
+        2 =>'INSERT INTO materia(nombre) VALUES(?)'
+    );
+    $preloader = new DBPreloader($paths, $sentences);
+}
+
+?>
