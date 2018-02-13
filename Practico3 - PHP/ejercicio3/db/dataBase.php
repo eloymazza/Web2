d28df9240e4afca8f97518ac04510cdc1d2d6f1d
Eloy Mazza <eloymazza.ra@gmail.com>
2018-02-12 22:08:43 -0300

    Agregue algo

--- Practico3 - PHP/ejercicio3/db/dataBase.php
+++ Practico3 - PHP/ejercicio3/db/dataBase.php
@@ -10,41 +10,39 @@ class DataBase{
 
 class DBPreloader extends DataBase{
 
-
     function __construct($paths, $sentences){
-        
         parent::__construct();
-        for ($i=0; $i < count($paths); $i++) { 
+        for ($i=0; $i < count($paths); $i++) {
             $this->precargar($paths[$i], $sentences[$i]);
         }
         fopen('dbStart/preloaded.txt', 'w+');
-    }      
+    }
 
     function precargar($path, $sentence){
-        $file = fopen($path, 'r');   
+        $file = fopen($path, 'r');
         $log = array();
-        
+
         while (!feof($file)){
             $log = explode(';',fgets($file));
             $this->insert($log,$sentence);
-        }    
+        }
         fclose($file);
     }
-    
+
     function insert($log,$sentenceSQL){
         $sentence = $this->db->prepare($sentenceSQL);
         $sentence->execute($log);
     }
-    
+
 }
 
 class DBInteractions extends DataBase{
-    
+
     function __construct(){
         parent::__construct();
 
     }
-    
+
     function getMaterias(){
 
         $sql = $this->db->prepare('SELECT * FROM materia');
@@ -57,14 +55,14 @@ class DBInteractions extends DataBase{
         $sql->execute([1]);
         $regAlumnos =  $sql->fetchAll(PDO::FETCH_ASSOC);
         $alumnos = array();
-        
+
         foreach ($regAlumnos as $regAlumno) {
             $sql = $this->db->prepare('SELECT * FROM alumno WHERE dni=?');
             $sql->execute([$regAlumno["dni"]]);
             $alumnos[] = $sql->fetchAll()[0];
         }
         return $alumnos;
-        
+
     }
 
     function getIDMateria($nombreMateria){
@@ -86,7 +84,7 @@ class DBInteractions extends DataBase{
         $id_alumno = $params[0];
         $id_materia = $this->getIDMateria($params[1]);
         $dia = $params[2];
-                
+
         $query = $this->db->prepare('INSERT INTO reg_asist(id_alumno, id_materia, dia) VALUES (?,?,?)');
         $query->execute([$id_alumno, $id_materia, $dia]);
     }
